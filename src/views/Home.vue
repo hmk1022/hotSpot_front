ca1b28f875fbb66d38d4e080ce75cab540c9046a
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-11-09 23:28:05 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+   
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+export default {
+  name: 'Home',
+  components: { },
+  methods: {
+      getData () {
+          axios.get(`http://api.kcisa.kr/openapi/service/rest/convergence2019/getConver01?serviceKey=${process.env.VUE_APP_SPOT}`).then((res)=>{
+            console.log('광광지 데이터 : ', res);
+          }).catch((err)=> {
+            console.log(err);
+          })
+      },
+  },
+  mounted(){
+    dotenv.config();
+    this.getData();
+  }
+  
+}
+</script>
